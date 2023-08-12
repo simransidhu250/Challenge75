24dc43c81c2b1da20c720251bae597c9ce0d9772
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-11 23:43:24 -0700

    Intial commit

---
+++ datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a =0;
+    cout<<"Size of the int: "<< sizeof(a)<<endl;
+    char b;
+    cout<<"Size of char: "<< sizeof(b)<<endl;
+    float c;
+    cout<<"Size of float: "<<sizeof(c)<<endl;
+    double d;
+    cout<<"Size of double: "<<sizeof(d)<<endl;
+
+    return 0;
+}

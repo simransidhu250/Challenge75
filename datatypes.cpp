80ea6d72863f8c6cc7c349c950fa22195c0d3a1e
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-14 12:16:04 -0700

    Size of inbuilt datatypes

--- datatypes.cpp
+++ datatypes.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
+#include <iostream>   //preprocessor directive used to include files and iostream is a header file for taking input and printing output
 using namespace std;
 
 int main(){
     int a =0;
-    cout<<"Size of the int: "<< sizeof(a)<<endl;
+    cout<<"Size of the int: "<< sizeof(a)<<endl; // Size of integer
     char b;
-    cout<<"Size of char: "<< sizeof(b)<<endl;
+    cout<<"Size of char: "<< sizeof(b)<<endl; // Size of character
     float c;
-    cout<<"Size of float: "<<sizeof(c)<<endl;
+    cout<<"Size of float: "<<sizeof(c)<<endl; // Size of float
     double d;
-    cout<<"Size of double: "<<sizeof(d)<<endl;
+    cout<<"Size of double: "<<sizeof(d)<<endl; //Size of double
+
+    short int si;
+    long int li;
+
+    cout<<"Size of shortint: "<<sizeof(si)<<endl; //Size of short integer
+    cout<<"Size of long int: "<<sizeof(li)<<endl; // Size of long integer
+
 
     return 0;
 }

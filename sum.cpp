6a9960cc989ca852a719540db9ac8da3b46ea665
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-14 17:10:20 -0700

    Adding two numbers

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int amt1, amt2, sum;
+    cout<<"Enter amount1:"; // First value
+    cin>>amt1;
+    cout<<"Enter amount2:"; // Second value 
+    cin>> amt2;
+    sum = amt1 + amt2;  // Total sum
+    cout<<"Sum is: "<<sum;  
+
+}

6374aea99b70ee98135e4f0823630e15d2cfa6a9
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-15 08:04:12 -0700

    Check if number is divisble by 3

--- divisible.cpp
+++ divisible.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     for(int i=1;i<=100;i++){
         if(i%3==0){
-            continue;
+            continue; // skips the numbers divisble by 3
         }
         cout<<i<<endl;
     }

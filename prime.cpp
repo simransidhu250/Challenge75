82cdfe63274c60945f44ee60dc885155d3b26c85
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-17 09:49:24 -0700

    Prime numbers in given range logic

--- prime.cpp
+++ prime.cpp
@@ -2,10 +2,23 @@
 using namespace std;
 
 int main(){
-    int n;
-    for(int i=2; i<n; i++){
-        if(n%i==0){
-            cout<<"The number is not prime";
+    // int n;
+    
+    // for(int i=2; i<n; i++){
+    //     if(n%i==0){
+    //         cout<<"The number is not prime"; 
+    //     }
+    // }
+    int j, start, end,k;
+    cin>>start>>end;
+    for(j=start; j<=end; j++){
+        for(k=2; k<j; k++){
+            if(j%k==0){
+                break;
+            }
+        }
+        if(k==j){
+            cout<<j<<"\t";
         }
     }
     return 0;

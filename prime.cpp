a4145ae225ffe68ae3a4265f7eca60759e08dea6
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-16 22:39:45 -0700

    printing prime numbers within a range

---
+++ prime.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            cout<<"The number is not prime";
+        }
+    }
+    return 0;
+}

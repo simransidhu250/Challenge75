cb58fcea89bfbb530a07285101c616cbb6cdd7e8
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-19 19:50:17 -0700

    hollow rectangle pattern

---
+++ pattern2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int rows=5;
+    int cols=4;
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=cols;j++){
+            if((i==1)||(i==rows)||(j==1)||(j==cols)){
+                cout<<"*\t";
+            }
+            else{
+                cout<<" \t";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}

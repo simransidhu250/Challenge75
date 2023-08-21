2f5f5a2eaaf711e647964c1938d3a7b106078e7b
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-21 12:30:41 -0700

    Butterfly pattern

--- pattern7.cpp
+++ pattern7.cpp
@@ -3,15 +3,39 @@ using namespace std;
 
 int main(){
     int n =4;
-    for(int i=1;i<=n;i++){
-        for(int j=1; j<=2*n; j++){
-            if((j<=i)||(j>(2*n-i))){
-                cout<<"*\t";
-            }
-            else{
-                cout<<" \t";
-            }
+    for(int i=n;i>=1;i--){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
         }
+
+        int space = (2*n-2*i);
+
+        for(int j=1; j<=space; j++){
+            cout<<" ";
+        }
+
+        for(int j=1; j<=i; j++){
+                    cout<<"*";
+                }
+        
+
+        // for(int j=1; j<=2*n; j++){
+        //     if((j<=i)||(j>(2*n-i))){
+        //         cout<<"*\t";
+        //     }
+        //     else{
+        //         cout<<" \t";
+        //     }
+        // }
+        //         for(int j=1; j<=2*n; j++){
+        //     if((j<=i)||(j>(2*n-i))){
+        //         cout<<"*\t";
+        //     }
+        //     else{
+        //         cout<<" \t";
+        //     }
+        // }
+
         cout<<endl;
     }
     return 0;

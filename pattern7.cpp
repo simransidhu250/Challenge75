5adc18cd3e41b3859d4913951eed074021ad6dd1
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-21 00:22:04 -0700

    Butterfly pattern

---
+++ pattern7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n =4;
+    for(int i=1;i<=n;i++){
+        for(int j=1; j<=2*n; j++){
+            if((j<=i)||(j>(2*n-i))){
+                cout<<"*\t";
+            }
+            else{
+                cout<<" \t";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+   

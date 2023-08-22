70a5d277f0671ded9d5090318f9770ae563622a4
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-21 18:11:16 -0700

    zig zag half

--- pattern14.cpp
+++ pattern14.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main(){
     int n=3;
     for(int i=1; i<=n; i++){
-        for(int j=n-i; j>=1; j--){
-            cout<<" ";
-        }
-        for(int j=1; j<=i; j++){
-            cout<<"*";
+        for(int j=1; j<=n; j++){
+            if(((i+j)%4==0) || (i==2 && j%4 ==0)){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
         }
         cout<<endl;
     }

8456b73c9b4689b5c067671d30031cc2c8f89e55
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-23 10:24:44 -0700

    Prime numbers in range using a function

--- primeno.cpp
+++ primeno.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 using namespace std;
 
-bool prime(int a, int b){
-    bool flag=true;
-    for(int i =a; i<=b;i++){
+void prime(int a, int b){
+    bool flag;
+    for(int i=a; i<=b;i++){
+        flag=0;
         for(int j=2; j<i; j++){
             if(i%j ==0){
-                flag = 0;
+                flag = 1;
+                break;
             }
+        
+        }
+        if(flag == 0){
+            cout<<i<<"\t";
         }
     }
 }
 
 int main(){
-    int a, b;
+    int a=2, b=20;
     prime(a,b);
     return 0;
 }

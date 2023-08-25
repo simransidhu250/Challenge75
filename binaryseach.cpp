42bdb3d1bfe47a4e6609624ef959b85e5761bda4
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-25 09:35:24 -0700

    Binary Search

---
+++ binaryseach.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {12, 15, 19, 21,23, 30};
+    int key = 23;
+    int end = 5;
+    int start = 0;
+    int mid = (start+end)/2;
+    if()
+    return 0;
+}

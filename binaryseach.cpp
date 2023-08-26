a6c6b8b07c02a9c18ee5d89860a1ae3896fbf7d4
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-25 22:46:56 -0700

    Binary Search Corrected

--- binaryseach.cpp
+++ binaryseach.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int arr[] = {12, 15, 19, 21,23, 30};
-    int key = 23;
-    int end = 5;
+int binary(int arr[],int n, int key){
     int start = 0;
-    int mid = (start+end)/2;
-    if()
-    return 0;
+    int end = n-1;
+    int mid;
+
+    while(start<=end){
+        mid = (start+end)/2;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(key<mid){
+            end = mid;
+        }
+        else{
+            start = mid;
+        }
+    }
+    return mid;
+}
+
+int main(){
+    int arr[] = {12,19,30,42,45,51};
+    int key = 45;
+    int len =  sizeof(arr)/sizeof(arr[0]);
+    int res = binary(arr, len, key);
+    cout<<res;
 }

207f3c59287a2b5503f84a657abd1a23d8236219
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-26 22:30:58 -0700

    Hexadecimal to Decimal Conversion

--- hexadecimaltodecimal.cpp
+++ hexadecimaltodecimal.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
-#include <cmath>
-// #include <string>
-
 using namespace std;
 
 int main(){
-    // Hexadecimal to Decimal number logic
-    // ICF to base 16 = 144 to base 8 
-
-    string n = "ICF";
-    int len = n.length()-1;
-    int res, power = 0;
-
-    for(int i= len; i>=0; i--){
-        if(('0'<=n[i]) && (n[i] <='9')){
-                res = pow(n[i], power) -'0'+ res;   
+    string str = "1CF";
+    int len = str.length();
+    int digit =0, res =0;
+    for(int i=len-1; i>= 0; i--){
+        if( ( '0'<str[i] ) && ('9'>str[i])){
+            digit = str[i] = '0';
+            res =
         }
-        else if (('A'<=n[i])&&('F'<=n[i])){
-
-        }
-        power++;
-    }
+    } 
     return 0;
 }

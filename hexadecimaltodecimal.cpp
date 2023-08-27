5cc480e452584516fc19b692f4c06139d9d2f9ad
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-08-27 10:38:19 -0700

    Hexadecimal to Decimal Conversion

--- hexadecimaltodecimal.cpp
+++ hexadecimaltodecimal.cpp
@@ -4,12 +4,19 @@ using namespace std;
 int main(){
     string str = "1CF";
     int len = str.length();
-    int digit =0, res =0;
+    int digit =0, res =0, x=1;
     for(int i=len-1; i>= 0; i--){
-        if( ( '0'<str[i] ) && ('9'>str[i])){
-            digit = str[i] = '0';
-            res =
+        if( ( '0'<=str[i] ) && ('9'>=str[i])){
+            digit = str[i] - '0';
+            res = res + digit*x;
+
         }
+        else if(('A'<=str[i]) && ('F'>=str[i])){
+            digit = str[i] - 'A' + 10;
+            res = res +digit*x;
+        }
+        x = x*16;
     } 
+    cout<< res;
     return 0;
 }

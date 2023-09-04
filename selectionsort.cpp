0b1ad0bdd6fbe17e5da714a811fadea19abb86f6
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-09-03 20:36:51 -0700

    Selection Sort

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -4,19 +4,17 @@ using namespace std;
 int main(){
     int arr[] = {5, 3,  9,  2, 1};
     int n = sizeof(arr)/ sizeof(0);
-    int min = 0;
     int temp;
     int i;
     for(i =0; i<n-1; i++){
-        min = i;
         for(int j=i+1; j<n ; j++){
-            if(arr[min]>arr[j]){
-                min = j;
+            if(arr[i]>arr[j]){
+                temp  = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
-        temp  = arr[i];
-        arr[i] = arr[min];
-        arr[min] = temp;
+
         cout<<arr[i]<<"\t";
     }
     cout<<arr[i]<<"\t";

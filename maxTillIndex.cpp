1e28dd3f0d4278c88753e85f30956259699e0403
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-09-10 18:20:07 -0700

    Finding maximum till particular index position of the array

---
+++ maxTillIndex.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {4,8,9,7,2,1,65,25};
+    int n = 4;
+    int max = INT_MIN;
+
+    // Finding max til a particular index
+    // Compare with the current element and check if the maximum is less than the current number 
+
+    for(int i=0; i<n; i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+    cout<<max;
+    return 0;
+}
